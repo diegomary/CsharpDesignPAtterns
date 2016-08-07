10ee3d953be06ed4cad0a8eb9155f54f5b2bfb21
Diego <Diego@DESKTOP-QFMB5AS>
2016-08-07 14:36:33 +0100

    Added Class CreateFactory

---
+++ AbstractFactory/Implementation/EmployerFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public class EmployerFactory
+    {
+        public AbstractEmployer CreateEmployer(EmployerType et)
+        {
+           
+            switch (et)
+            {
+                case EmployerType.BusinessAnalyst:
+                return new BuisineesAnalyst();
+                case EmployerType.Developer:
+                return new Developer();
+                case EmployerType.Manager:
+                return new Manager();
+                // If no case label contains a matching value, control is transferred to the default section
+                default:
+                return new Manager();
+            }
+
+        }
+    }
+}
--- AbstractFactory/PilotApp/Program.cs
+++ AbstractFactory/PilotApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AbstractFactory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace PilotApp
     {
         static void Main(string[] args)
         {
+
+            EmployerFactory ep = new EmployerFactory();
+            Manager mg = (Manager)ep.CreateEmployer(EmployerType.Manager);
+
+            mg.ManagerialStatus = "CTO";
+
+            var f = mg.ManagerialStatus;
+
+
+            var t = 0;
+          
+
         }
     }
 }
